10fa8f2ab2a34622a969703834499d8b8b418538
envikaaa <envikapriyatna31@gmail.com>
2026-03-06 15:37:41 +0700

    void display

--- BubbleShort.cpp
+++ BubbleShort.cpp
@@ -3,3 +3,34 @@ using namespace std;
 
 int a[20];
 int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan panjang array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break
+        } 
+        else 
+        {
+            cout << error : Maksimal input array adalah 20/n";
+        
+        }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen array" << endl;
+    cout << "=====================" << endl;
+    
+    for (int i = 0; 1< n i++) // i++ / i + 1
+    {
+        cout << "Masukkan nilai ke- " << 1 + 1 << " : ";
+        cin << a[i];
+    }
+    void display
+
+    }
+}
